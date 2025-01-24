e7b965d501be623e5de276595f25292000abbeb3
Priya Raj <144319984+satyam969@users.noreply.github.com>
2025-01-24 15:53:39 +0530

    Update auth.js
    logout issue

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -54,7 +54,7 @@ const options = {
   },
    cookies: {
     sessionToken: {
-      name: `__Secure-next-auth.session-token`,
+      name: `next-auth.session-token`,
       options: {
         httpOnly: true,
         sameSite: "none", 
